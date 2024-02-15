bc0c1bef3dbf20d48a748268f9a6618be112fb9c
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-15 16:58:50 +0100

    create script with ajax requesr for quotes

---
+++ script.js
@@ -0,0 +1,25 @@
+function ajaxRequest() {
+	$('.quote-text .loader').show();
+	$.ajax({
+		url: "https://smileschool-api.hbtn.info/quotes",
+		method: "GET",
+		dataType: "json",
+		success: function (data) {
+			processData(data);
+			$('.quote-text .loader').hide();
+		}
+	})
+}
+
+function processData(data) {
+	let carouselItems = $(".carousel-item");
+	$(data).each(function (index, item) {
+		let carouselItem = $(carouselItems[index]);
+		carouselItem.find("img").attr("src", item.pic_url);
+		carouselItem.find("p").text(item.text);
+    carouselItem.find("h4").text(item.name);
+    carouselItem.find("span").text(item.title);
+	})
+}
+
+ajaxRequest();
