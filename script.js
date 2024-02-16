a6d525e48a3b689797fb8a9648a690e3b4579989
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-02-16 16:54:41 +0100

    ajax load for latest videos section

--- script.js
+++ script.js
@@ -59,5 +59,43 @@ function processDataTutorials(data) {
 	})
 }
 
+function ajaxRequestVideos() {
+	$('#carouselExampleControls3').hide();
+	$('.loader-videos').show()
+	$.ajax({
+		url: "https://smileschool-api.hbtn.info/latest-videos",
+		method: "GET",
+		dataType: "json",
+		success: function (data) {
+			processDataVideos(data);
+			$('#carouselExampleControls3').show();
+			$('.loader-videos').hide()
+		}
+	})
+}
+
+function processDataVideos(data) {
+	let cards = $('.card-video');
+
+	$(data).each(function (index, item) {
+		let card = $(cards[index]);
+
+		card.find(".card-img-top").attr("src", item.thumb_url)
+		card.find(".card-title").text(item.title)
+		card.find(".card-text").text(item["sub-title"])
+		card.find(".rounded-circle").attr("src", item.author_pic_url)
+		card.find("h6").text(item.author)
+		let rating = card.find("div.rating")
+
+		for (let i = 0; i < item.star; i++) {
+			let starImage = $("<img>", { src: "images/star_on.png", alt: "star on", width: "15px" });
+			rating.append(starImage);
+		}
+
+		card.find("span").text(item.duration);
+	})
+}
+
 ajaxRequestQuotes();
 ajaxRequestTutorials();
+ajaxRequestVideos();
